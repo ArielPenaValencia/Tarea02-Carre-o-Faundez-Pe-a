d85806f5196ae95cf91da74422e6bac353216fc7
ArielPeñaValencia <ariel.pena@alumnos.uv.cl>
2018-12-13 22:46:14 -0400

    tarea 2

---
+++ home/user/tarea2/src/echoServerSrc/TCPEchoServer.cc
@@ -0,0 +1,104 @@
+/*
+ *   C++ sockets on Unix and Windows
+ *   Copyright (C) 2002
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "YASL.h"      // For Socket, ServerSocket, and SocketException
+#include "checkArgs.h"
+#include <iostream>    // For cerr and cout
+#include <cstdlib>     // For atoi()
+#include <fstream>
+
+const uint32_t RCVBUFSIZE = 32;    // Size of receive buffer
+
+// TCP client handling function
+void HandleTCPClient(TCPSocket *sock) {
+	std::cout << "Handling client ";
+	try {
+		std::cout << sock->getForeignAddress() << ":";
+	} catch (SocketException e) {
+		std::cerr << "Unable to get foreign address" << std::endl;
+			
+	}
+	try {
+		std::cout << sock->getForeignPort();
+	} catch (SocketException e) {
+		std::cerr << "Unable to get foreign port" << std::endl;
+		
+	}	
+	std::cout << std::endl;
+	// Send received string and receive again until the end of transmission
+
+	//status 
+	sock->send("HTTP/1.1 200 OK\r\n",17);
+	sock->send("Content-Type: text/html\r\n\r\n",27);
+ 	std:: string line;
+ 	std:: string obte_html="";
+ 	std:: string obte_error="";
+ 	std:: ifstream html ("UNO.html");
+ 	std:: ifstream error ("404.html");
+	//lectura de html
+ 	if (html.is_open()){
+ 		while(getline(html,line)){
+ 			obte_html = obte_html + line + "\n";
+		}
+ 		html.close();
+ 	}else{
+		//lectura de 404
+	 	if (error.is_open()){
+	 		while(getline(error,line)){
+	 			obte_error = obte_error + line + "\n";
+			}
+			error.close();
+		}
+ 	}
+ 	//envio html
+ 	sock->send(obte_html.c_str(),obte_html.length());
+
+ 	delete sock;
+
+	//char echoBuffer[RCVBUFSIZE];
+	//uint32_t recvMsgSize;
+	//while ((recvMsgSize = sock->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means
+	                                                 // end of transmission
+		// Echo message back to client
+		//sock->send(echoBuffer, recvMsgSize);
+
+	//}
+	//delete sock;
+}
+
+int main(int argc, char *argv[]) {
+
+	checkArgs* argumentos = new checkArgs(argc, argv);
+	uint16_t echoServPort;   
+	echoServPort  = argumentos->getArgs().PORT;
+
+	try {
+		TCPServerSocket servSock(echoServPort);     // Server Socket object
+
+		for (;;) {   // Run forever
+			HandleTCPClient(servSock.accept());       // Wait for a client to connect
+		}
+	} catch (SocketException &e) {
+		std::cerr << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	// NOT REACHED
+
+	return EXIT_SUCCESS;
+}
